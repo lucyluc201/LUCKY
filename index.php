aa6889b64b8ca5d8a65b3fb49161047d2ea9bb9e
lucyluc201 <2023akcs857f@kab.ac.ug>
2024-09-21 05:00:17 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$name="AINOMUGISHA  LUCY";
+$DOB="01/02/2001";
+$Address=" NTUNGAMO";
+$CYear=2024;
+$BYear=2001;
+$Age=$CYear-$BYear;
+echo"Name: ".$name. "<br>";
+echo"Date Of Birth : ".$DOB."<br>";
+echo"Home Address:".$Address."<br>";
+echo"Welcome home: ".$name."<br>";
+echo"You are now , ".$Age." Years old";
+/* DOB represents date of birth,
+CYEAR means current year
+BYEAR means year of birth*/
+?> 
+
